f23e83d30aa92c36d289a38b6b96734426bc5cb9
Louis David Jean Magarshack <magarsha@icbc07pc40.epfl.ch>
2013-03-05 14:13:47 +0100

    Added the Mockito library for generating mocks in tests.

--- src/ch/epfl/unison/test/RobotiumTest.java
+++ src/ch/epfl/unison/test/RobotiumTest.java
@@ -1,10 +1,18 @@
 package ch.epfl.unison.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+
+import org.mockito.Mockito;
+
 import android.test.ActivityInstrumentationTestCase2;
 
 import ch.epfl.unison.ui.LoginActivity;
 
 import com.jayway.android.robotium.solo.Solo;
+
+import static org.mockito.Mockito.*;
 
 public class RobotiumTest extends ActivityInstrumentationTestCase2<LoginActivity>{
 
@@ -13,6 +21,8 @@ public class RobotiumTest extends ActivityInstrumentationTestCase2<LoginActivity
 	}
 
 	public void testRobotium(){
+		URLConnection mockCo = mock(URLConnection.class);
+		
 		Solo solo = new Solo(getInstrumentation(), getActivity());
 		
 		//Assumes you had already logged in so that it is now done automatically.
