a171d1399e25f29bfd6014698600ee80f7deb2ce
Vincent Jalil <vincent.jalil@epfl.ch>
2013-03-30 19:04:53 +0100

    Added one more test for the player and some new mock responses.
    CAUTION: the test added has been commented because it depends on your library. You have to customize it bebore you run it.
    
    CAUTION: For an unknown reason, the groupsActivity is started before the mainActivity. This might be the cause of tests not passing when you run them all together. Try running them separately.

--- src/ch/epfl/unison/test/MainActivityTest.java
+++ src/ch/epfl/unison/test/MainActivityTest.java
@@ -7,9 +7,12 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.ProtocolVersion;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.message.BasicHttpResponse;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -18,9 +21,12 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 import ch.epfl.unison.Const;
 import ch.epfl.unison.Const.PrefKeys;
+import ch.epfl.unison.R;
 import ch.epfl.unison.api.HttpClientFactory;
+import ch.epfl.unison.api.JsonStruct;
 import ch.epfl.unison.mockUtils.MockResponses;
 import ch.epfl.unison.ui.MainActivity;
 
@@ -39,6 +45,9 @@ public class MainActivityTest extends
 	private final Long uid = Long.valueOf(0);
 
 	private Intent intent;
+	
+	private boolean isDJ = false;
+	private int skippedSongsNumber = 0;
 
 	public MainActivityTest() {
 		super(MainActivity.class);
@@ -48,6 +57,12 @@ public class MainActivityTest extends
 	protected void setUp() throws Exception {
 
 		mockResponses = new MockResponses();
+		
+		JsonStruct.Group group = new JsonStruct.Group();
+		group.nbUsers = 0;
+		group.gid = gid;
+		group.distance = null;
+		group.name = gname;
 
 		SharedPreferences prefs = PreferenceManager
 				.getDefaultSharedPreferences(getInstrumentation()
@@ -62,11 +77,12 @@ public class MainActivityTest extends
 
 		intent = new Intent(getInstrumentation().getTargetContext(),
 				MainActivity.class).putExtra(Const.Strings.GID, gid).putExtra(
-				Const.Strings.NAME, gname);
+				Const.Strings.NAME, gname).putExtra(Const.Strings.GROUP, group);
 	}
 
 	public void testTabsNav() throws ClientProtocolException, IOException,
 			InterruptedException {
+		
 		HttpClient mockClient = mock(HttpClient.class);
 
 		when(mockClient.execute((HttpUriRequest) anyObject())).thenAnswer(
@@ -79,7 +95,7 @@ public class MainActivityTest extends
 						if (input.getURI().getPath() == null || input.getURI().getPath().endsWith("groups")) {
 							return mockResponses.groupsGETSuccess;
 						} else {
-							return mockResponses.mainGETGroupsSuccess;
+							return mockResponses.mainGETGroupsNotDJSuccess;
 						}
 					}
 				});
@@ -90,9 +106,11 @@ public class MainActivityTest extends
 
 		assertTrue(solo.waitForActivity("MainActivity"));
 
-		solo.clickOnText("STATS");
-
-		solo.clickOnText("MUSIC");
+		solo.clickOnText(solo.getString(R.string.fragment_title_stats));
+		
+		
+		solo.clickOnText(solo.getString(R.string.fragment_title_player));
+		
 
 		solo.finishOpenedActivities();
 	}
@@ -112,7 +130,7 @@ public class MainActivityTest extends
                         } else if (input.getURI().getPath().endsWith("nickname")){
                             return mockResponses.PUTNicknameSuccess;
                         } else {
-                            return mockResponses.mainGETGroupsSuccess;
+                            return mockResponses.mainGETGroupsNotDJSuccess;
                         }
                     }
                 });
@@ -139,11 +157,136 @@ public class MainActivityTest extends
         
         solo.finishOpenedActivities();
     }
+	
+	//We can make this test more precise by looking at the entity of the request with path "current"
+	//and see whether the song that is being played is indeed the one we expected.
+	//CAUTION: order is important in the Answer implementation
+	//CAUTION: This test depends on your music library. You have to customize this test and the responses in
+	//         MockResponses.java
+	
+	//TODO - Handle POST requests to groupstreamer.com/libentries/0/batch
+	//     - Make different cases when a track is being played so that it is shown when we fetch the group info
+	//       from the server. ( GET request to /groups/0 )
+	//     - Test the seekbar (Robotium can wait for a log. It might be useful).
+
+//	public void testPlayer() throws ClientProtocolException, IOException, InterruptedException {
+//	        
+//		 HttpClient mockClient = mock(HttpClient.class);
+//		 
+//		    when(mockClient.execute((HttpUriRequest) anyObject())).thenAnswer(
+//	                new Answer<HttpResponse>() {
+//	                    @Override
+//	                    public HttpResponse answer(final InvocationOnMock invocation)
+//	                            throws Throwable {
+//	                        HttpUriRequest input = (HttpUriRequest) invocation.getArguments()[0];
+//	                        String requestPath = input.getURI().getPath();
+//	                        if (requestPath == null) {
+//	                        	return new BasicHttpResponse(new ProtocolVersion(
+//										"HTTP", 1, 1), HttpStatus.SC_BAD_REQUEST, "Bad request, die! Request path was null ");
+//	                        }
+//	                        
+//	                        if (requestPath.endsWith("groups")) {
+//	                            return mockResponses.groupsGETSuccess;
+//	                        } else if (requestPath.endsWith("master")){
+//	                        	isDJ = true;
+//	                            return mockResponses.PUTMasterSuccess;
+//	                        } else if (requestPath.endsWith("playlist")) {
+//	                        	return mockResponses.GETPlaylistSuccess;
+//	                        } else if (requestPath.contains("tracks")){
+//	                        	switch (skippedSongsNumber) {
+//	                        	case 0: return mockResponses.GET1st2ndTrack;
+//	                        	case 1: return mockResponses.GET2nd3rdTrack;
+//	                        	case 2: return mockResponses.GET3rdTrack;
+//	                        	case 3: return  mockResponses.GETNoMoreTracks;
+//	                        	default:
+//	                        		return new BasicHttpResponse(new ProtocolVersion(
+//											"HTTP", 1, 1), HttpStatus.SC_BAD_REQUEST, "Bad request, die! Problem with number of skipped tracks.");
+//	                        	}
+//	                        }  else if (requestPath.endsWith("current")) {
+//	                        	return mockResponses.PUTCurrentSuccess;
+//	                        } else if (requestPath.contains("groups/")){
+//	                            if (isDJ) {
+//	                            	return mockResponses.mainGETGroupsDJSuccess;
+//	                            } else {
+//	                            	return mockResponses.mainGETGroupsNotDJSuccess;
+//	                            }
+//	                        } else {
+//	                        	return new BasicHttpResponse(new ProtocolVersion(
+//										"HTTP", 1, 1), HttpStatus.SC_BAD_REQUEST, "Bad request, die! Asked : " + input.getURI().getPath());
+//	                        }
+//	                    }
+//	                });
+//	        
+//	        HttpClientFactory.setInstance(mockClient);
+//	        
+//	        getActivity().startActivity(intent);
+//	        Solo solo = new Solo(getInstrumentation());
+//	        
+//	        assertTrue(solo.waitForActivity("MainActivity"));
+//	        Button playButton = (Button) solo.getView(R.id.musicToggleBtn);
+//	        Button nextButton = (Button) solo.getView(R.id.musicNextBtn);
+//	        Button prevButton = (Button) solo.getView(R.id.musicPrevBtn);
+//	        
+//	        
+//	        solo.clickOnText("Become the DJ");
+//	        
+//	        //let the servercom finish
+//	        Thread.sleep(3000);
+//	        
+//	        
+//	        solo.clickOnView(playButton);
+//
+//	        //Enjoy the music :)
+//	        Thread.sleep(3000);
+//	        
+//	        assertTrue(solo.waitForText("Rihanna"));
+//	        
+//	        skippedSongsNumber ++;
+//	        solo.clickOnView(nextButton);
+//	        
+//	        Thread.sleep(3000);
+//	        
+//	        assertTrue(solo.waitForText("Bruno"));
+//	        
+//	        Thread.sleep(2000);
+//	        
+//	        skippedSongsNumber ++;
+//	        solo.clickOnView(nextButton);
+//	        
+//	        Thread.sleep(6000);
+//	        
+//	        //click twice because we want to hear the previous song.
+//	        solo.clickOnView(prevButton);
+//	        solo.clickOnView(prevButton);
+//	        
+//	        Thread.sleep(2000);
+//	        
+//	        
+//	        //If you hear that, then the pause button is working :)
+//	        solo.clickOnView(playButton);
+//	        Thread.sleep(500);
+//	        solo.clickOnView(playButton);
+//	        Thread.sleep(500);
+//	        solo.clickOnView(playButton);
+//	        Thread.sleep(500);
+//	        solo.clickOnView(playButton);
+//	        Thread.sleep(500);
+//	        solo.clickOnView(playButton);
+//	        Thread.sleep(500);
+//	        
+//	        solo.finishOpenedActivities();
+//	        
+//	}
+	
+	
 
 	@Override
 	protected void tearDown() {
+		isDJ = false;
+		skippedSongsNumber = 0;
+		
 		SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(getActivity());
+				.getDefaultSharedPreferences(getInstrumentation().getTargetContext());
 		Editor editor = prefs.edit();
 		editor.clear();
 		editor.commit();
